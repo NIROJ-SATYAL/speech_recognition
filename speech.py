1b8265ab1362f233d633da66cde71b1096f945dd
NIROJ-SATYAL <69862735+NIROJ-SATYAL@users.noreply.github.com>
2021-05-12 17:05:47 +0545

    Create speech.py

---
+++ speech.py
@@ -0,0 +1,17 @@
+import speech_recognition as sr
+
+
+listener=sr.Recognizer()
+
+try:
+
+
+    with sr.Microphone() as source:
+        print("listening.....")
+        voice=listener.listen(source)
+        command=listener.recognize_google(voice)
+        print(command)
+
+except:
+    print("say again")
+
